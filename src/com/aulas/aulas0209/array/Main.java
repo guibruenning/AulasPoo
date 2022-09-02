5a747c220e8ae4a63d17639797be997956e709e9
Guilherme Bruenning <bruenning.guilherme@gmail.com>
2022-09-02 09:00:07 -0300

    Aulas POO

--- src/com/aulas/aulas0209/array/Main.java
+++ src/com/aulas/aulas0209/array/Main.java
@@ -3,11 +3,6 @@ package com.aulas.aulas0209.array;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numeros = new int[5];
-        numeros[0] = 1;
-        numeros[1] = 4;
-        numeros[2] = 6;
-        numeros[3] = 8;
-        numeros[4] = 11;
+        int[] numeros = MinhasFuncoes.criaVetor(5);
     }
 }
