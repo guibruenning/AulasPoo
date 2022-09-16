699887f2a0699be0cda5b094b17a63e8a772f44c
Guilherme Bruenning <bruenning.guilherme@gmail.com>
2022-09-16 10:43:18 -0300

    Aulas dia 16-09

--- src/com/aulas/aulas1609/teste/Bola.java
+++ src/com/aulas/aulas1609/teste/Bola.java
@@ -1,7 +1,36 @@
 package com.aulas.aulas1609.teste;
 
 public class Bola {
-    String tipo;
-    String material;
+    private String tipo;
+    private String material;
+    private double preco;
+
+
+    public void setTipo(String tipo) {
+        if(tipo.length() > 0)
+            this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setMaterial(String material) {
+        if(material.length() > 0)
+            this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setPreco(double preco) {
+        if(preco > 0)
+            this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 
 }
--- src/com/aulas/aulas1609/teste/BolaMain.java
+++ src/com/aulas/aulas1609/teste/BolaMain.java
@@ -3,5 +3,13 @@ package com.aulas.aulas1609.teste;
 public class BolaMain {
     public static void main(String[] args) {
         Bola bola = new Bola();
+
+        bola.setMaterial("Fibra") ;
+        bola.setTipo("Basquete");
+        bola.setPreco(23) ;
+
+        System.out.println("Material: "+bola.getMaterial());
+        System.out.println("Tipo: "+bola.getTipo());
+        System.out.println("Preço: R$ "+bola.getPreco());
     }
 }
